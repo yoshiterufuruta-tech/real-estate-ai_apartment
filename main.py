4823733c3608fbd4485848d61873b300b2912d22
yoshiterufuruta-tech <yoshiteru.furuta@gmail.com>
2026-07-01 05:27:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,8 @@ def predict(req: PredictRequest):
     raw["駅距離_log"] = np.log1p(raw["駅距離"])
     raw["面積_sqrt"] = np.sqrt(raw["面積"])
 
-    X = preprocess.transform(raw)
+    X = preprocess(raw)
+    X = X[model.feature_name_in_]
     pred = model.predict(X)[0]
     pred = pred * (122.1 / 119.2)
     pred_list_price = pred * 1.255
